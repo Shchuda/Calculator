0b8ea9ffe6029c0ba39f053ad8766ac4a4009be6
Shchuda <137898720+Shchuda@users.noreply.github.com>
2024-02-15 21:02:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const display = document.getElementById("display");
+const display = document.getElementById("display"); // Get reference to the display element
 
 function appendToDisplay(input) {
-    display.value += input
+    display.value += input; // Append the input value to the display
 }
 
 function clearDisplay() {
-    display.value = "";
+    display.value = ""; // Clear the display
 }
 
 function calculate() {
     try {
-        display.value = eval(display.value)
+        display.value = eval(display.value); // Evaluate the expression in the display and set the result
     }
     catch(error) {
-        display.value = "Error"
+        display.value = "Error"; // Handle any errors and display "Error"
     }
 }
+
